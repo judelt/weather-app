fa53f1bac2e47c84c7beb66814c1bd97ea6829e4
judelt <mendez.puig@gmail.com>
2021-07-27 11:36:43 -0700

    refactor

--- src/hooks/useApplicationData.jsx
+++ src/hooks/useApplicationData.jsx
@@ -22,57 +22,55 @@ export default function useApplicationData() {
     lat: "",
   });
 
-  function getCurrentWeatherandCoordinates(city) {
-    axios.get(`${api.base}/weather?q=${city}&appid=${api.key}`).then((res) => {
-      setState((prev) => ({
-        ...prev,
-        currentWeather: res.data.weather[0],
-        lon: res.data.coord.lon,
-        lat: res.data.coord.lat,
-      }));
-    });
+  async function getCurrentWeatherandCoordinates(city) {
+    const { data } = await axios.get(
+      `${api.base}/weather?q=${city}&appid=${api.key}`
+    );
+    setState((prev) => ({
+      ...prev,
+      currentWeather: data.weather[0],
+      lon: data.coord.lon,
+      lat: data.coord.lat,
+    }));
   }
 
-  function getForecast() {
-    axios
-      .get(
-        `${api.base}/onecall?lat=${state.lat}&lon=${state.lon}&appid=${api.key}`
-      )
-      .then((res) => {
-        setState((prev) => ({
-          ...prev,
-          forecast: res.data.daily,
-        }));
-      });
+  async function getForecast() {
+    const { data } = await axios.get(
+      `${api.base}/onecall?lat=${state.lat}&lon=${state.lon}&appid=${api.key}`
+    );
+
+    setState((prev) => ({
+      ...prev,
+      forecast: data.daily,
+    }));
   }
 
   async function searchByCity(city) {
     try {
-      getCurrentWeatherandCoordinates(city)
-      .then(() => getForecast())
+      await getCurrentWeatherandCoordinates(city);
+      getForecast();
     } catch (err) {
       setError(err);
     }
   }
 
-  function getCity(zip) {
-    axios
-      .get(`${zipApi.base}/search?apikey=${zipApi.key}&codes=${zip}`)
-      .then((res) => {
-        setZipCity(res.data.results[zip][0].city);
-        console.log("city", res.data.results[zip][0].city);
-      })
+  async function getCity(zip) {
+    const { data } = await axios.get(
+      `${zipApi.base}/search?apikey=${zipApi.key}&codes=${zip}`
+    );
+    setZipCity(data.results[zip][0].city);
+    // console.log("city", data.results[zip][0].city);
   }
 
-  async function searchByZip (zip) {
+  async function searchByZip(zip) {
     try {
-      getCity(zip)
-        .then(() => getCurrentWeatherandCoordinates(zipCity))
-        .then(() => getForecast())
+      await getCity(zip);
+      await getCurrentWeatherandCoordinates(zipCity);
+      getForecast();
     } catch (err) {
       setError(err);
     }
   }
-
+  // console.log('state', state)
   return { searchByCity, searchByZip, state, error };
 }
