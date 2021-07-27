987e283faf48533963570524a899b18b351fd8ed
judelt <mendez.puig@gmail.com>
2021-07-27 09:12:24 -0700

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,24 @@
-import { useState, KeyboardEvent } from "react";
-import axios from "axios";
+import { useState } from "react";
+import useApplicationData from "./hooks/useApplicationData";
 import "./App.css";
 
-const api = {
-  key: process.env.REACT_APP_WEATHER_API_KEY,
-  base: process.env.REACT_APP_WEATHER_API_BASE,
-};
-
 function App() {
+  const { searchByCity } = useApplicationData();
   const [city, setCity] = useState("");
-  
-  const [state, setState] = useState({
-    currentWeather: "",
-    forecast: [],
-    lon: "",
-    lat: "",
-  });
-  const [error, setError] = useState("");
-
-  const searchByCity = (e: KeyboardEvent): void => {
-    if (e.key === "Enter") {
-      axios
-        .get(`${api.base}/weather?q=${city}&appid=${api.key}`)
-        .then((res) => {
-          setState((prev) => ({
-            ...prev,
-            currentWeather: res.data.weather[0],
-            lon: res.data.coord.lon,
-            lat: res.data.coord.lat,
-          }));
-        })
-        .then(() => {
-          axios
-            .get(
-              `${api.base}/onecall?lat=${state.lat}&lon=${state.lon}&appid=${api.key}`
-            )
-            .then((res) => {
-              console.log("res.data.daily", res.data.daily);
-              setState((prev) => ({
-                ...prev,
-                forecast: res.data.daily,
-              }));
-            });
-        })
-        .catch((err) => setError(err));
-    }
-  };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Weather app</h1>
       </header>
-      <body>
         <input
           type="text"
-          placeholder="Search for city..."
+          placeholder="Search by city..."
           value={city}
           onChange={(e) => setCity(e.target.value)}
-          onKeyPress={searchByCity}
+          onKeyPress={(e) => searchByCity(e, city)}
         ></input>
-      </body>
-    </div>
+      </div>
   );
 }
 
