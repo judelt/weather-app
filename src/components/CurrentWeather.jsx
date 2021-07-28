103e7ea558f880e692467d80cb2d34a030638f81
judelt <mendez.puig@gmail.com>
2021-07-27 18:41:47 -0700

    reorganize

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -1,35 +1,19 @@
 import useApplicationData from "../hooks/useApplicationData";
 
-function CurrentWeather() {
-  const { state } = useApplicationData();
-  const {
-    currentWeather,
-    wind_speed,
-    cityName,
-    sys,
-    temp,
-    temp_max,
-    temp_min,
-    humidity,
-  } = state;
-  const iconURL = `http://openweathermap.org/img/wn/${currentWeather.icon}@2x.png`;
-
+function CurrentWeather({cityName, country, temp, description, icon}) {
+  const iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
   return (
     <>
-      {cityName && (
-        <>
-          <div className="location">
-            {cityName}, {sys.country}
-          </div>
-          <div className="weather-container">
-            <div className="temp">{Math.round(temp - 273.15)}°C</div>
-            <div className="description">{currentWeather.description}</div>
-            <div className="weather animate">
-              <img src={iconURL} alt="icon"></img>
-            </div>
-          </div>
-        </>
-      )};
+      <div className="location">
+        {cityName}, {country}
+      </div>
+      <div className="weather-container">
+        <div className="temp">{Math.round(temp - 273.15)}°C</div>
+        <div className="description">{description}</div>
+        <div className="weather animate">
+          <img src={iconURL} alt="icon"></img>
+        </div>
+      </div>
     </>
   );
 }
